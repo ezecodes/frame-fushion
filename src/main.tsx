2778a52afc6d40d81cb2cf50f78f0987316155b9
ezecodes <>
2024-04-05 13:01:17 +0100

    add routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,48 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {RouterProvider, createBrowserRouter} from "react-router-dom"
+import Dashboard from './pages/Dashboard.tsx'
+import Login from './pages/Login.tsx'
+import Signup from './pages/Signup.tsx'
+import Main from './pages/Main.tsx'
+import Surveillance from './pages/Surveillance.tsx'
+import ActivityLogs from './pages/ActivityLogs.tsx'
 
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />
+  },
+  {
+    path: "/app",
+    element: <App />,
+    children: [
+      {
+        path: "/app",
+        element: <Dashboard />
+      },
+      {
+        path: "/app/surveillance",
+        element: <Surveillance />
+      },
+      {
+        path: "/app/logs",
+        element: <ActivityLogs />
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Signup />
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={route} />
   </React.StrictMode>,
 )
