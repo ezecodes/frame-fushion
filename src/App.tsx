5da56eff3616ec3aae672e3fdfb75710d76ef638
ezecodes <>
2024-04-05 13:01:49 +0100

    add sidebars

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from "react-router-dom"
+import AppHeader from "./components/AppHeader"
+import Sidebar from "./components/Sidebar"
 
 function App() {
 
   return (
-    <>
-       
-    </>
+    <section className='min-h-[100vh] flex flex-col'>
+      <AppHeader />
+      <section className="bg-raisinBlack flex-1 flex">
+        <Sidebar />
+        <Outlet />
+      </section>
+    </section>
   )
 }
 
